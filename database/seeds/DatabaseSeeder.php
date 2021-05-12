df7cd708895ed6cae782869c531c3e09f8142bfa
Tomcze Tutunowski <t.tutunowski@gmail.com>
2021-05-12 20:47:27 +0200

    creating administrator model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AdminsTableSeeder::class);
+
         // $this->call(UsersTableSeeder::class);
     }
 }
