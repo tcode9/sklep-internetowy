07fec0f97fef4bd86da924c39edd546bbb8df4bf
Tomcze Tutunowski <t.tutunowski@gmail.com>
2021-06-02 09:22:39 +0200

    including cart functionality, cart controller and view

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers\Site;
 
+use Cart;
 use Illuminate\Http\Request;
 use App\Contracts\ProductContract;
 use App\Http\Controllers\Controller;
 use App\Contracts\AttributeContract;
-
 
 class ProductController extends Controller
 {
     protected $productRepository;
 
     protected $attributeRepository;
-
 
     public function __construct(ProductContract $productRepository, AttributeContract $attributeRepository)
     {
@@ -31,6 +30,11 @@ class ProductController extends Controller
 
     public function addToCart(Request $request)
     {
-        dd($request->all());
+        $product = $this->productRepository->findProductById($request->input('productId'));
+        $options = $request->except('_token', 'productId', 'price', 'qty');
+
+        Cart::add(uniqid(), $product->name, $request->input('price'), $request->input('qty'), $options);
+
+        return redirect()->back()->with('message', 'Item added to cart successfully.');
     }
 }
